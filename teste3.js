cb5c6285eaf089f327f853db79a0de26b6ef325a
Imac <imac@iMac-de-Imac.local>
2023-06-13 19:17:30 -0300

    adjust bug in indexOF

--- teste3.js
+++ teste3.js
@@ -1,7 +1,7 @@
 const data =  require("./fakeData");
 
 module.exports = function(req, res) {
-  
+
     const { name } =  req.query;
 
     const user = data.find((user) => user.name === name);
@@ -9,7 +9,9 @@ module.exports = function(req, res) {
     if (!user) {
         return res.status(404).json({error: 'Usuário não encontrado'})
     }
-    data.splice(data.indexOf(user.name), 1)
+    const index = data.indexOf(user)
+
+    data.splice(index, 1)
 
     return res.status(200).json({message: `O usuário "${user.name}" foi removido com sucesso`});
 
